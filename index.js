ff4c46e8f1ceaa5b6251770e72eca20ae476a901
Olena Molostova <olena.molostova@devico.io>
2022-01-25 13:26:08 +0200

    added some functions

---
+++ index.js
@@ -0,0 +1,86 @@
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+const shortid = require('shortid');
+const url = require('url');
+const querystring = require('querystring');
+const todosPath = path.resolve('./db/todos.json');
+
+const host = 'localhost';
+const port = 8000;
+
+const todos = require('./db/todos.json');
+
+const requestListener = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  if (req.url === '/todos' && req.method === 'GET') {
+    res.writeHead(200);
+    res.end(JSON.stringify(todos));
+  } else if (req.url === '/todos' && req.method === 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body += data;
+    });
+    req.on('end', function () {
+      let text = JSON.parse(body);
+      fs.readFile(todosPath)
+        .then(data => {
+          const todos = JSON.parse(data);
+
+          JSON.stringify(
+            todos.push({
+              id: shortid(),
+              description: text,
+              completed: false,
+            }),
+          );
+          fs.writeFile(todosPath, JSON.stringify(todos, null, 2));
+          res.writeHead(200);
+          res.end(JSON.stringify(todos));
+        })
+        .catch(err => console.log(err.message));
+    });
+  } else if (req.method === 'DELETE') {
+    const parsed = url.parse(req.url);
+    const query = querystring.parse(parsed.query);
+    fs.readFile(todosPath, 'utf-8')
+      .then(data => {
+        const todos = JSON.parse(data);
+
+        const newTodos = todos.filter(item => item.id !== query.id);
+        fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
+        res.writeHead(200);
+        res.end(JSON.stringify(todos));
+      })
+      .catch(err => console.log(err.message));
+    // } else if (req.url === '/todos' && req.method === 'PUT') {
+    //   updateTodo(req.body);
+    //   res.writeHead(200);
+    //   res.end(JSON.stringify(todos));
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: 'Not found' }));
+  }
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+// const todosRouter=require("./")
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/todos", todosRouter);
+
+// const PORT = process.env.PORT || 8000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
