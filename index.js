710099e4555ca5668dfae276c098cb46f80c9a44
Olena Molostova <olena.molostova@devico.io>
2022-01-27 13:11:52 +0200

    fixed functions

--- index.js
+++ index.js
@@ -11,90 +11,122 @@ const host = 'localhost';
 const port = 8000;
 
 const requestListener = (req, res) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Content-Type', 'application/json');
-  if (req.url === '/todos' && req.method === 'GET') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200);
-    res.end(JSON.stringify(todos));
-  } else if (req.url === '/todos' && req.method === 'POST') {
-    let body = '';
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      let text = JSON.parse(body);
-      fs.readFile(todosPath)
-        .then(data => {
-          const todos = JSON.parse(data);
-          JSON.stringify(
-            todos.push({
-              id: shortid(),
-              description: text,
-              completed: false,
-            }),
-          );
-          fs.writeFile(todosPath, JSON.stringify(todos, null, 2));
-          res.setHeader('Access-Control-Allow-Origin', '*');
-          res.setHeader('Content-Type', 'application/json');
-          res.writeHead(200);
-          res.end(JSON.stringify(todos));
-        })
-        .catch(err => console.log(err.message));
-    });
-    res.writeHead(200);
-    res.end(JSON.stringify(todos));
-  } else if (req.method === 'DELETE') {
-    const parsed = url.parse(req.url);
-    const query = querystring.parse(parsed.query);
-    fs.readFile(todosPath, 'utf-8')
-      .then(data => {
-        const todos = JSON.parse(data);
-        const newTodos = todos.filter(item => item.id !== query.id);
-        fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(200);
-        res.end(JSON.stringify(newTodos));
-      })
-      .catch(err => console.log(err.message));
-  } else if (req.method === 'PUT') {
-    const parsed = url.parse(req.url);
-    const query = querystring.parse(parsed.query);
-    let body = '';
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      let text = JSON.parse(body);
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Request-Method': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': '*',
+  };
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if (['GET', 'POST', 'DELETE', 'PUT'].indexOf(req.method) > -1) {
+    res.writeHead(200, headers);
+    if (req.url === '/todos' && req.method === 'GET') {
+      res.writeHead(200, headers);
+      res.end(JSON.stringify(todos));
+    } else if (req.url === '/todos' && req.method === 'POST') {
+      let body = '';
+      req.on('data', function (data) {
+        body += data;
+      });
+      req.on('end', function () {
+        let text = JSON.parse(body);
+        fs.readFile(todosPath)
+          .then(data => {
+            const todos = JSON.parse(data);
+            JSON.stringify(
+              todos.push({
+                id: shortid(),
+                description: text,
+                completed: false,
+              }),
+            );
+            fs.writeFile(todosPath, JSON.stringify(todos, null, 2));
+            res.writeHead(200, headers);
+            res.end(JSON.stringify(todos));
+          })
+          .catch(err => console.log(err.message));
+      });
+    } else if (req.method === 'DELETE') {
+      const parsed = url.parse(req.url);
+      const query = querystring.parse(parsed.query);
       fs.readFile(todosPath, 'utf-8')
         .then(data => {
           const todos = JSON.parse(data);
-          console.log('queryid', query.id);
-          const todoToUpdate = todos.find(todo => todo.id === query.id);
-          const newTodo = {
-            id: todoToUpdate.id,
-            description: text,
-            completed: todoToUpdate.completed,
-          };
-          const newTodos = todos.map(todo => {
-            if (todo.id === query.id) {
-              todo = newTodo;
-            }
-            return todo;
-          });
+          const newTodos = todos.filter(item => item.id !== query.id);
           fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
-          res.setHeader('Access-Control-Allow-Origin', '*');
-          res.setHeader('Content-Type', 'application/json');
-          res.writeHead(200);
+
+          res.writeHead(200, headers);
           res.end(JSON.stringify(newTodos));
         })
         .catch(err => console.log(err.message));
-    });
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ message: 'Not found' }));
+    } else if (req.url === '/todos/clear-completed') {
+      fs.readFile(todosPath, 'utf-8')
+        .then(data => {
+          const todos = JSON.parse(data);
+          const newTodos = todos.filter(item => item.completed === false);
+          fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
+          res.end(JSON.stringify(newTodos));
+        })
+        .catch(err => console.log(err.message));
+    } else if (req.method === 'PUT') {
+      let body = '';
+      req.on('data', function (data) {
+        body = data;
+      });
+      req.on('end', function () {
+        let parsedBody = JSON.parse(body);
+        fs.readFile(todosPath, 'utf-8')
+          .then(data => {
+            const todos = JSON.parse(data);
+            const todoToUpdate = todos.find(todo => todo.id === parsedBody.id);
+            const newTodo = {
+              ...todoToUpdate,
+              ...parsedBody,
+            };
+            const newTodos = todos.map(todo => {
+              if (todo.id === todoToUpdate.id) {
+                todo = newTodo;
+              }
+              return todo;
+            });
+            fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
+            res.writeHead(200, headers);
+            res.end(JSON.stringify(newTodos));
+          })
+          .catch(err => console.log(err.message));
+      });
+    } else if (req.url === '/todos/toggle-completed') {
+      fs.readFile(todosPath, 'utf-8')
+        .then(data => {
+          const todos = JSON.parse(data);
+          const isAnyActive = todos.some(todo => todo.completed === false);
+          let newTodos;
+          if (isAnyActive) {
+            newTodos = todos.map(todo => {
+              todo.completed = true;
+              return todo;
+            });
+          } else {
+            newTodos = todos.map(todo => {
+              todo.completed = false;
+              return todo;
+            });
+          }
+          fs.writeFile(todosPath, JSON.stringify(newTodos, null, 2));
+          res.end(JSON.stringify(newTodos));
+        })
+        .catch(err => console.log(err.message));
+    } else {
+      res.writeHead(404);
+      res.end(JSON.stringify({ message: 'Not found' }));
+    }
   }
 };
 
