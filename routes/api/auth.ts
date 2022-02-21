f111d3c6290344a46abde09dbd5d0c7126674ede
Olena Molostova <olena.molostova@devico.io>
2022-02-21 18:25:29 +0200

    login part1

--- routes/api/auth.ts
+++ routes/api/auth.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import { BadRequest, Conflict } from 'http-errors';
+import { BadRequest, Conflict, Unauthorized } from 'http-errors';
 import bcrypt from 'bcryptjs';
 
 import { collections } from '../../service/database';
 import { User } from '../../model/user';
-import { joiSchema } from '../../model/user';
+import { joiRegisterSchema, joiLoginSchema } from '../../model/user';
 
 export const authRouter = express.Router();
 
 authRouter.post('/register', async (req, res, next) => {
   try {
-    const { error } = joiSchema.validate(req.body);
+    const { error } = joiRegisterSchema.validate(req.body);
     if (error) {
       throw new BadRequest(error.message);
     }
@@ -34,4 +34,22 @@ authRouter.post('/register', async (req, res, next) => {
   }
 });
 
-authRouter.post('/login', async (req, res, next) => {});
+authRouter.post('/login', async (req, res, next) => {
+  try {
+    const { error } = joiLoginSchema.validate(req.body);
+    if (error) {
+      throw new BadRequest(error.message);
+    }
+    const { email, password } = req.body;
+    const user = await collections.users.findOne({ email });
+    if (!user) {
+      throw new Unauthorized('Email or password is wrong!');
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw new Unauthorized('Email or password is wrong!');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
